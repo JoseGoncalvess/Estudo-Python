ca36c4eeefc009f4ac2b1eea46cdf9d9aa4029ac
José Gonçalves <joseinacio774@gmail.com>
2022-10-24 10:22:42 -0300

    ajust by crator strutura

---
+++ Projeto em Python/Desktop-Action/Automação-Wokspace.py
@@ -0,0 +1,10 @@
+##Instalação do Selenium mais o WEbdriver(new-version)##
+from selenium import webdriver #import webdriver
+from webdriver_manager.chrome import ChromeDriverManager #importwebdrive-maneger
+from selenium.webdriver.chrome.service import Service #import service do selenium
+from selenium.webdriver.common.by import By
+
+#Estrutura Montada
+servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=servico)
+navegador.get("https://www.google.com/webhp?hl=pt-BR&sa=X&ved=0ahUKEwiVhJGQ-fj6AhXUnpUCHUOFDwUQPAgI")
